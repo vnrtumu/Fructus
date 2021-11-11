2be9901177a2ee54b8f2e5f15d60853dedcc0e73
venkat reddy <vnr.tumu@gmail.com>
2021-11-11 19:26:05 +0530

    Integrated onboarding with dynamic data

--- Fructus/App/OnboardingView.swift
+++ Fructus/App/OnboardingView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct OnboardingView: View {
     //MARK: - Properties
+    var fruits: [Fruit] = fruitsData
     
     //MARK: - Body
     var body: some View {
         TabView{
-            ForEach(0..<5){ item in
-                FruitCardView()
+            ForEach(fruits[0...5]){ item in
+                FruitCardView(fruit: item)
             }//:LOOP
            
         }//: TAB
@@ -27,6 +28,7 @@ struct OnboardingView: View {
 //MARK: - Preview
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(fruits: fruitsData)
+            .previewDevice("iPhone 13 Pro Max")
     }
 }
--- Fructus/Views/FruitCardView.swift
+++ Fructus/Views/FruitCardView.swift
@@ -11,24 +11,26 @@ struct FruitCardView: View {
     //MARK: - Properties
     @State private var isAnimating:Bool = false
     
+    var fruit: Fruit
+    
     //MARK: - Body
     var body: some View {
         ZStack {
             VStack(spacing: 20) {
                 //FRUIT: IMAGE
-                Image("blueberry")
+                Image(fruit.image)
                     .resizable()
                     .scaledToFit()
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
                     .scaleEffect(isAnimating ? 1.0 : 0.6)
                 //FRUIT: TITLE
-                Text("blue berries")
+                Text(fruit.title)
                     .foregroundColor(Color.white)
                     .font(.largeTitle)
                     .fontWeight(.heavy)
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity:  0.15), radius: 2, x: 2, y: 2)
                 //FRUIT: HEADLINE
-                Text("Blueberries are sweet, nutritious and widly popular fruit all over the  world")
+                Text(fruit.headline)
                     .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 16)
@@ -44,7 +46,7 @@ struct FruitCardView: View {
             }
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0,  maxHeight: .infinity, alignment: .center)
-        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"),Color("ColorBlueberryDark") ]), startPoint: .top, endPoint: .bottom))
+        .background(LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(20)
         .padding(.horizontal, 20)
         
@@ -55,7 +57,7 @@ struct FruitCardView: View {
 //MARK: - Preview
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitCardView()
+        FruitCardView(fruit: fruitsData[1])
             .previewLayout(.fixed(width: 320, height: 640))
     }
 }
