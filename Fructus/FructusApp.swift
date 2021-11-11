39335720a4109b33bb3dacc1269ef7501de060ea
venkat reddy <vnr.tumu@gmail.com>
2021-11-11 16:18:57 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by venkat reddy on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
